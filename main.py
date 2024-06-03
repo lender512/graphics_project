7a53b7bdc071ea478f3c258d1a5954f9febae36d
lberrospir <luis.berrospi@entel.pe>
2024-06-03 17:19:48 -0500

    owo

--- main.py
+++ main.py
@@ -1,79 +1,63 @@
 import cv2
 import numpy as np
-import imageio
-import noise
 import random
 import time
 from particle_system import ParticleSystem, Constraint
 
-
 WIDTH = 800
 HEIGHT = 600
 
-
-def generate_test (width, height, num_particles):
+def generate_test(width, height, num_particles):
     test = []
     for i in range(num_particles):
         row = []
         for j in range(num_particles):
-            row.append([j*width/num_particles, i*height/num_particles])
+            row.append([j * width / num_particles, i * height / num_particles])
         test.append(row)
     return test
 
-test = generate_test(500, 500, 10)
+test = generate_test(500, 500, 20)
 
-ps = ParticleSystem(len(test[0])*len(test) , np.array([0, 50], dtype=np.float64), .2)
-
-
-
+ps = ParticleSystem(len(test[0]) * len(test), np.array([0, 1000], dtype=np.float64), 0.2)
 
 for i in range(len(test)):
     for j in range(len(test[i])):
-        ps.m_x[i*len(test[i])+j] = test[i][j]
-        ps.m_oldx[i*len(test[i])+j] = test[i][j]
+        ps.m_x[i * len(test[i]) + j] = test[i][j]
+        ps.m_oldx[i * len(test[i]) + j] = test[i][j]
 
-l = 10    
-
+l = 20    
+constraints = []
 for i in range(len(test)):
     for j in range(len(test[i])):
         if i > 0:
-            ps.m_constraints.append(Constraint(i*len(test[i])+j, (i-1)*len(test[i])+j, l))
-        if i < len(test)-1:
-            ps.m_constraints.append(Constraint(i*len(test[i])+j, (i+1)*len(test[i])+j, l))
+            constraints.append(Constraint(i * len(test[i]) + j, (i - 1) * len(test[i]) + j, l))
+        if i < len(test) - 1:
+            constraints.append(Constraint(i * len(test[i]) + j, (i + 1) * len(test[i]) + j, l))
         if j > 0:
-            ps.m_constraints.append(Constraint(i*len(test[i])+j, i*len(test[i])+j-1, l))
-        if j < len(test[i])-1:
-            ps.m_constraints.append(Constraint(i*len(test[i])+j, i*len(test[i])+j+1, l))
-1
-            
-    
-        
+            constraints.append(Constraint(i * len(test[i]) + j, i * len(test[i]) + j - 1, l))
+        if j < len(test[i]) - 1:
+            constraints.append(Constraint(i * len(test[i]) + j, i * len(test[i]) + j + 1, l))
 
+ps.addConstraint(constraints)
 
 t = time.time()
+start_time = time.time()
+frame_count = 0
 while True:
-
     img = np.zeros((HEIGHT, WIDTH, 3), dtype=np.uint8)
-
-
-
     for i in range(len(ps.m_x)):
-        # substract height from y to flip the image
-        cv2.circle(img, np.rint(ps.m_x[i][:2]).astype(
-            int), 2, (255, 255, 255), -1)
-    
+        cv2.circle(img, np.rint(ps.m_x[i][:2]).astype(int), 2, (255, 255, 255), -1)
     for c in ps.m_constraints:
-        cv2.line(img, np.rint(ps.m_x[c.particleA][:2]).astype(int), np.rint(
-            ps.m_x[c.particleB][:2]).astype(int), (255, 255, 255), 1)
+        cv2.line(img, np.rint(ps.m_x[c.particleA][:2]).astype(int), np.rint(ps.m_x[c.particleB][:2]).astype(int), (255, 255, 255), 1)
         
+    cv2.putText(img, f"FPS: {frame_count / (time.time() - start_time):.2f}", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+    
     cv2.imshow('img', img)
     cv2.setMouseCallback('img', ps.mouseEvent)
     
-    
-
-    ps.TimeStep(time.time()-t if t < 0.1 else 0.1)
+    ps.TimeStep(time.time() - t)
     t = time.time()
-      
+    frame_count += 1
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
--- particle_system.py
+++ particle_system.py
@@ -1,71 +1,57 @@
-
-
-
 import numpy as np
 import cv2
 
-NUM_ITERATIONS = 10
+NUM_ITERATIONS = 3
 
 class Constraint:
-    
-    
-    def __init__(yo, particleA, particleB, restlength):
-        yo.particleA = particleA
-        yo.particleB = particleB
-        yo.restlength = restlength
+    def __init__(self, particleA, particleB, restlength):
+        self.particleA = particleA
+        self.particleB = particleB
+        self.restlength = restlength
 
 class ParticleSystem:
-    
-    defaultPosA = [0, 0]
-    defaultPosB = [500, 0]
-    
-    
-    def __init__(yo, num_particles, gravity, time_step):
-        yo.m_x = np.zeros((num_particles, 2), dtype=np.float64)
-        yo.m_oldx = np.zeros((num_particles, 2), dtype=np.float64)
-        yo.m_a = np.zeros((num_particles, 2), dtype=np.float64)
-        yo.m_vGravity = gravity
-        yo.m_fTimeStep = time_step
-        yo.m_constraints = []
-        
-        
-        
-    def TimeStep(yo, dt):
-        yo.m_fTimeStep = dt
-        yo.AccumulateForces()
-        yo.Verlet()
-        yo.SatisfyConstraints()
-        
-    def Verlet(yo):
-        for i in range(len(yo.m_x)):
-            temp = yo.m_x[i].copy()
-            yo.m_x[i] += yo.m_x[i] - yo.m_oldx[i] + yo.m_a[i] * yo.m_fTimeStep * yo.m_fTimeStep
-            yo.m_oldx[i] = temp.copy()
-            
-    def SatisfyConstraints(yo):
-        for j in range(NUM_ITERATIONS):
-            
-            for i in range(len(yo.m_constraints)):
-                constraint = yo.m_constraints[i]
-                delta = yo.m_x[constraint.particleB] - yo.m_x[constraint.particleA]
-                deltaLength = np.linalg.norm(delta)
-                diff = (deltaLength - constraint.restlength) / deltaLength
-                yo.m_x[constraint.particleA] += delta * 0.2 * diff
-                yo.m_x[constraint.particleB] -= delta * 0.2 * diff
-            yo.m_x[0] = np.array(yo.defaultPosA, dtype=np.float64)
-            yo.m_x[9] = np.array(yo.defaultPosB, dtype=np.float64)
-    
-            
-                
-                
-    def AccumulateForces(yo):
-        # Accumulate forces
-        for i in range(len(yo.m_x)):
+    defaultPosA = np.array([0, 0], dtype=np.float64)
+    defaultPosB = np.array([500, 0], dtype=np.float64)
 
-            yo.m_a[i] = yo.m_vGravity
-            
-    def mouseEvent(yo, event, x, y, flags, param):
+    def __init__(self, num_particles, gravity, time_step):
+        self.m_x = np.zeros((num_particles, 2), dtype=np.float64)
+        self.m_oldx = np.zeros((num_particles, 2), dtype=np.float64)
+        self.m_a = np.zeros((num_particles, 2), dtype=np.float64)
+        self.m_vGravity = gravity
+        self.m_fTimeStep = time_step
+        self.m_constraints = []
+
+    def addConstraint(self, constraints):
+        self.m_constraints = constraints
+
+    def addParticles(self, particles):
+        self.m_x = particles
+        self.m_oldx = particles
+
+    def TimeStep(self, dt):
+        self.m_fTimeStep = dt
+        self.AccumulateForces()
+        self.Verlet()
+        self.SatisfyConstraints()
+
+    def Verlet(self):
+        self.m_x, self.m_oldx = self.m_x + (self.m_x - self.m_oldx + self.m_a * self.m_fTimeStep ** 2), self.m_x.copy()
+
+    def SatisfyConstraints(self):
+        for _ in range(NUM_ITERATIONS):
+            for constraint in self.m_constraints:
+                delta = self.m_x[constraint.particleB] - self.m_x[constraint.particleA]
+                deltaLength = np.linalg.norm(delta)
+                diff = (deltaLength - constraint.restlength) / deltaLength if deltaLength > 0 else 0
+                self.m_x[constraint.particleA] += delta * 0.5 * diff
+                self.m_x[constraint.particleB] -= delta * 0.5 * diff
+            self.m_x[0], self.m_x[19] = self.defaultPosA, self.defaultPosB
+
+    def AccumulateForces(self):
+        self.m_a[:] = self.m_vGravity
+
+    def mouseEvent(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
-            yo.defaultPosA = [x, y]
+            self.defaultPosA = np.array([x, y], dtype=np.float64)
         if event == cv2.EVENT_RBUTTONDOWN:
-            yo.defaultPosB = [x, y]
+            self.defaultPosB = np.array([x, y], dtype=np.float64)
